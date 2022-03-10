52e9c801f9c3a6a8645f70cdf52cc4ccefd74451
AntonyMorgante <antonymorgante1@gmail.com>
2022-03-10 16:35:14 +0100

    buttons

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,13 +3,18 @@
 @section("title","Fumetto")
 
 @section("content")
-    <div class="container">
+    <div class="container my-4">
         <h4>{{$comic->title}}</h4>
         <img src="{{$comic->thumb}}" alt="">
-        <p>{{$comic->description}}</p>
+        <p class="pt-4">{{$comic->description}}</p>
         <p>Serie: {{$comic->series}}</p>
         <p>Disponibile da: {{$comic->sale_date}}</p>  
         <p>Prezzo: {{$comic->price}}</p>
-        <p></p>
-    <a href="{{route("comics.index")}}">Torna alla lista dei fumetti</a>
+    <a href="{{route('comics.index')}}"><button type="button" class="btn btn-primary">Torna alla lista dei fumetti</button></a>
+    <a href="{{route('comics.edit', $comic->id)}}"><button type="button" class="btn btn-primary">Modifica</button></a>
+    <form action="{{route('comics.destroy', $comic->id)}}" method="POST" class="d-inline-block">
+        @csrf
+        @method("DELETE")
+        <button type="submit" class="btn btn-danger">Elimina</button>
+    </form>
     </div> 
