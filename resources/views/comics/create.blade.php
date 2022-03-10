9dd0f132865491ac41e705c24abdf4310f7a9b13
AntonyMorgante <antonymorgante1@gmail.com>
2022-03-10 16:30:37 +0100

    crud + fixed seeder

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,41 @@
+@extends("layouts.base")
+
+@section("title","Aggiungi un fumetto")
+
+@section("content")
+<div class="container">
+    <form action="{{route('comics.store')}}" method="POST">
+
+        @csrf
+
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Inserisci il titolo del fumetto...">
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Descrizione</label>
+            <input type="text" class="form-control" id="description" name="description" placeholder="Inserisci una breve descrizione">
+        </div>
+        <div class="mb-3">
+            <label for="thumb" class="form-label">Copertina</label>
+            <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Inserisci l'indirizzo dell'immagine">
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Prezzo</label>
+            <input type="text" class="form-control" id="price" name="price" placeholder="Inserisci il prezzo di vendita">
+        </div>
+        <div class="mb-3">
+            <label for="series" class="form-label">Serie</label>
+            <input type="text" class="form-control" id="series" name="series" placeholder="Inserisci la serie a cui appartiene il fumetto">
+        </div>
+        <div class="mb-3">
+            <label for="sale_date" class="form-label">Data di uscita</label>
+            <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Inserisci la data di uscita">
+        </div>
+        <div class="mb-3">
+            <label for="type" class="form-label">Genere</label>
+            <input type="text" class="form-control" id="type" name="type" placeholder="Inserisci il genere a cui appartiene il fumetto">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,15 +4,28 @@
 
 @section("content")
 <div class="container">
+    <div class="text-center my-4">
+        <a href="{{route('comics.create')}}"><button type="button" class="btn btn-primary">Aggiungi un fumetto</button></a>
+    </div>
     <div class="row">
         @foreach ($comics as $comic)
-            <div class="comic">
-                <h4>{{$comic->title}}</h4>
-                <img src="{{$comic->thumb}}" alt="">
-                <p>Serie: {{$comic->series}}</p>
-                <p>Prezzo: {{$comic->price}}</p>
-                <a href="{{route('comics.show', $comic->id)}}">Vedi in dettaglio</a>
-            </div>
+            <div class="col-3">
+                <div class="text-center">
+                    <h4>{{$comic->title}}</h4>
+                    <img src="{{$comic->thumb}}" alt="">
+                    <p>Serie: {{$comic->series}}</p>
+                    <p>Prezzo: {{$comic->price}}</p>
+                </div>
+                <div class="text-center">
+                    <a href="{{route('comics.show', $comic->id)}}"><button type="button" class="btn btn-primary">Dettagli</button></a>
+                        <a href="{{route('comics.edit', $comic->id)}}"><button type="button" class="btn btn-primary">Modifica</button></a>
+                        <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                            @csrf
+                            @method("DELETE")
+                            <button type="submit" class="btn btn-danger">Elimina</button>
+                        </form>
+                    </div>
+                </div>
         @endforeach        
     </div>
 </div>
